2a982723f464dd045e2bb08e2e02cd13387c65c8
jrg94 <jrg170@case.edu>
2015-11-03 20:59:49 -0500

    Initial Commit

---
+++ include/as5.h
@@ -0,0 +1,13 @@
+/**
+ * Author: Jeremy Griffith
+ *
+ * The overall header file for the program
+ */
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define NUM_OF_THREADS 200
---
+++ src/main.c
@@ -0,0 +1,11 @@
+/**
+ * @Author: Jeremy Griffith
+ *
+ * The entry file for the program
+ */
+
+#include "as5.h"
+
+int main(int argc, char* argv[]) {
+	pthread_t threads[NUM_OF_THREADS];
+}
