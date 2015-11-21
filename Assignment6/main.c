f70d2b69818c0f7340f59b5e7e4ffe975f47a9d7
jrg94 <jrg170@case.edu>
2015-11-21 13:47:03 -0500

    Got a decent run of the code -> Tina is eating all the cookies though

--- Assignment6/main.c
+++ Assignment6/main.c
@@ -10,6 +10,9 @@ int main(int argc, char* argv[]) {
 	// Test input
 	if (argc != 2) {
 		// Print error
+		printf("Not enough inputs\n");
+		printf("Try: ./<executable> <server>\n");
+		exit(EXIT_FAILURE);
 	}
 
 	// Create client
@@ -17,12 +20,13 @@ int main(int argc, char* argv[]) {
 	c = clnt_create(server, COOKIE_JAR, COOKIE_JAR_VERSION, "udp");
 	if (c == NULL) {
 		// Print error & exit
+		clnt_pcreateerror("Could not create client");
 		exit(EXIT_FAILURE);
 	}
 
 	// Begin asking for cookies
 	while (1) {
-		printf("Requesting a cookie");
+		printf("Requesting a cookie\n");
 		int response = askForCookies(c);
 		
 		// Cookie jar is empty
@@ -32,12 +36,12 @@ int main(int argc, char* argv[]) {
 		}
 		// Judy has to wait
 		else if (response == -1) {
-			printf("Judy must wait until Tina has 2 cookies");
+			printf("Judy must wait until Tina has 2 cookies\n");
 			continue;
 		}
 		// Free to take a cookie
 		else if (response == 1) {
-			printf("Got a cookie");
+			printf("Got a cookie\n");
 			continue;
 		}
 		
--- Assignment6/src/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-clear
-rpcgen cookies.x
-make
-ssh -f jrg170@eecslinab2 "./tina"
-ssh -f jrg170@eecslinab3 "./judy"
-ssh -f jrg170@eecslinab7 "./mother"
